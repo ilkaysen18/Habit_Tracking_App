f480ca4865854f18c0ebd854838a5fb678f1888f
Ilkay Rachel Lee Sen <ilkaysen18@gmail.com>
2026-09-15 21:22:34 +0300

    Update completion_log.py

--- Habits/models/completion_log.py
+++ Habits/models/completion_log.py
@@ -1,11 +1,23 @@
 """
-Completion Log:
+"COMPLETION LOG" CLASS - MODEL PACKAGE:
+This Module defines the Attributes for tracking timestamps regarding user habit completion
+and broken habits.      """
 
-This  module holds the "CompletionLog" Class (or Entity). This Class tracks the individual
-timestamps for users, regarding habit completion and broken habits.
 """
 
-from datetime import datetime, time
+
+class Habit:
+  """
+  This Class represents the habits that users create to track their habits and/or tasks.
+  Attributes:
+    habit_id (int): PK - This Primary Key is the Habit Class' (relational) unique identifier.
+    habit_name (str): This Attribute is for the habit/task's name.
+    periodicity (str): frequency - 'daily', 'weekly', 'biweekly', 'fortnightly', 'monthly', 'yearly'.
+    created_at (datetime): Metadata record for tracking the timestamp of habit creation.
+    edited_at (datetime): Similar to above; tracks the time a habit was edited.
+    reminder_time (time): A configuration that tracks user alerts.
+  """
+
 
 
     
