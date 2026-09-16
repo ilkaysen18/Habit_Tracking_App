b05b3239e4d12f316d6f1b5e5e86ee2ede62d1b9
Ilkay Rachel Lee Sen <ilkaysen18@gmail.com>
2026-09-16 16:38:24 +0300

    Update db_manager.py

--- Habits/database/db_manager.py
+++ Habits/database/db_manager.py
@@ -12,7 +12,7 @@ from datetime import datetime, timedelta
 DB_PATH = "tracker.db"
 
 def get_connection():
-    """ Initializes and returns connection instances to the SQLite Database. """
+    """Initializes and returns connection instances to the SQLite Database."""
     return sqlite3.connect(DB_PATH)
 
 
@@ -49,7 +49,7 @@ def initialize_tables() -> None:
 
 
 def seed_predefined_fixtures() -> None:
-    """ Automates the 5 Predefined Test Fixtures and generates 4-Week Test Data. """"
+    """Automates the 5 Predefined Test Fixtures and generates 4-Week Test Data.""""
     with get_connection() as conn:
         cursor = conn.cursor()
 
