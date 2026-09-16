74cca9382ad7c4a758507fa93b2b129ce54c79b9
Ilkay Rachel Lee Sen <ilkaysen18@gmail.com>
2026-09-16 23:09:16 +0300

    Update main.py

--- Habits/main.py
+++ Habits/main.py
@@ -91,9 +91,34 @@ def create_new_habit_flow() -> None:
     
     periodicity = period_specifications.get(p_choice)
     
-    if not periodicity:
-        print("❌ Invalid input.")
-        return
+    # regarding habit creation for the code below.
+    # approves correct input for periodicity, saves to completion log.
+    # if invalid input, prints error script.
+
+    	for idx, h in enumerate(habits):
+		print(f"{idx + 1}. {h.habit_name} [{h.periodicity}]")
+
+	try:
+		selection = int(input("\nSelect habit index row to create: ")) - 1
+		if selection < 0 or selection >= len(habits):
+            		raise IndexError
+	
+        	target_habit = habits[selection]
+        	now_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+		target_periodicity = period_specifications[selection]
+
+		with get_connection() as conn:
+			cursor = conn.cursor()
+			cursor.execute(
+				INSERT INTO completion_logs (user_id, habit_id, created_at, periodicity)
+				VALUES (?, ?, ?);
+					(user_name.user_id, target_habit.habit_id, now_str, p_choice.periodicity_id)
+			conn.commit() 
+
+		        print("✅ New habit and time frame saved! '{target_habit.habit_name}' set for {target_periodicity}.")
+	
+	except (ValueError, IndexError):
+        	print("❌ Invalid input.")
 
 
 def check_off_habit_flow(habits: list) -> None:
