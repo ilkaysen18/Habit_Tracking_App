9897701c7e557fe820a1c4505b03d39eb3118587
Ilkay Rachel Lee Sen <ilkaysen18@gmail.com>
2026-09-18 16:24:13 +0300

    Update predefined_habits.py

--- Habits/database/predefined_habits.py
+++ Habits/database/predefined_habits.py
@@ -18,3 +18,48 @@ from modules.analytics import (
 )
 
 
+def five_predefined_habits() -> None:
+    """These are the 5 Predefined Habits."""
+    with get_connection() as conn:
+        cursor = conn.cursor()
+
+    # Prevents duplication:
+    cursor.execute("SELECT COUNT(*) FROM habits;")
+    if cursor.fetchone()[0] > 0:
+        return
+
+    import sys
+    from main import create_new_habit_flow
+
+    period_specifications = {
+        "1": "daily",
+        "2": "weekly",
+        "3": "biweekly",
+        "4": "fortnightly",
+        "5": "monthly",
+        "6": "yearly"
+    }
+
+    periodicity = period_specifications.get(p_choice)
+    if not periodicity:
+        print("❌ Invalid periodicity choice.")
+        return
+
+    now_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # 1. Defines the 5 Data Records:
+    predefined_habits = [
+        ("Drink 2L water", "daily"),
+        ("Go to the gym", "daily"),
+        ("Read 10 pages", "daily"),
+        ("Wash the car", "weekly"),
+        ("Submit weekly timesheet", "weekly")
+    ]
+
+    for name, periodicity in five_predefined_habits:
+        cursor.execute("""
+            INSERT INTO habits (habit_name, periodicity, created_at, edited_at)
+            VALUES (?, ?, ?, ?);
+        """, (name, periodicity, start_date.strftime("%Y-%m-%d %H:%M:%S"), start_date.strftime("%Y-%m-%d %H:%M:%S")))
+        habit_id = cursor.lastrowid
+        conn.commit()
