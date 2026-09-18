6ee3998ada5581b8b64e3e7cee3c5d72de8d0050
Ilkay Rachel Lee Sen <ilkaysen18@gmail.com>
2026-09-18 04:49:18 +0300

    Update main.py

--- Habits/main.py
+++ Habits/main.py
@@ -155,10 +155,10 @@ def check_off_habit_flow(habits: list) -> None:
 def completed_habits(habit_id: int, periodicity: str) -> bool:
     now = datetime.now()
 
-    if periodicity.lower() == "daily":
-		from datetime import datetime, timedelta
-        start = now.replace(hour=0, minute=0, second=0, microsecond=0)
-        end = start + timedelta(days=1)
+if periodicity.lower() == "daily":
+from datetime import datetime, timedelta
+start = now.replace(hour=0, minute=0, second=0, microsecond=0)
+end = start + timedelta(days=1)
 
     elif periodicity.lower() == "weekly":
         # week window: from Monday 00:00 to next Monday 00:00
