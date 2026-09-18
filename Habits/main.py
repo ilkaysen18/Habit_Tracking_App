a6cfba033edad6fb61bdd59b043b83d70b01918b
Ilkay Rachel Lee Sen <ilkaysen18@gmail.com>
2026-09-18 04:42:01 +0300

    Update main.py

--- Habits/main.py
+++ Habits/main.py
@@ -156,6 +156,7 @@ def completed_habits(habit_id: int, periodicity: str) -> bool:
     now = datetime.now()
 
     if periodicity.lower() == "daily":
+		from datetime import datetime, timedelta
         start = now.replace(hour=0, minute=0, second=0, microsecond=0)
         end = start + timedelta(days=1)
 
