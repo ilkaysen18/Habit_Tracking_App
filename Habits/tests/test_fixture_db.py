0bb9f93c946b5b76b17a831a86f58649b927285d
Ilkay Rachel Lee Sen <ilkaysen18@gmail.com>
2026-09-19 00:41:23 +0300

    Update test_fixture_db.py

--- Habits/tests/test_fixture_db.py
+++ Habits/tests/test_fixture_db.py
@@ -6,35 +6,43 @@ This is a separate DB for the Test Fixture of 4-Weeks Dummy Data for the 5 Prede
 
 
 
+# Imports SQLite3 for the DB.
 import sqlite3
+
+
+
+# Definitions below.
+
 
 def get_test_connection(db_name: str = "test_fixture.db"):
     return sqlite3.connect(db_name)
+
 
 def initialize_test_tables(db_name: str = "test_fixture.db"):
 
     with sqlite3.connect(db_name) as conn:
         cursor = conn.cursor()
 
-        # IMPORTANT:
-        # Replace these table definitions with THE SAME DATA TYPES AS MAIN DB.
+        # Habits Table for the Test Fixtures.
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS habits (
                 habit_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                habit_name TEXT NOT NULL,
-                periodicity TEXT NOT NULL,
-                created_at TEXT NOT NULL,
-                edited_at TEXT NOT NULL
+                habit_name STRING NOT NULL,
+                periodicity STRING NOT NULL,
+                created_at DATETIME NOT NULL,
+                edited_at DATETIME NOT NULL
             );
         """)
 
+        # Completion Logs Table for the Test Fixtures.
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS completion_logs (
                 log_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 habit_id INTEGER NOT NULL,
-                completed_at TEXT NOT NULL,
+                completed_at STRING NOT NULL,
                 FOREIGN KEY (habit_id) REFERENCES habits(habit_id)
             );
         """)
 
+        # Must commit to finalize above details.
         conn.commit()
