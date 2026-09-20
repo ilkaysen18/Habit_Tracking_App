6e4dde28147e62aece9ba56ee960ae5c5acfba29
Ilkay Rachel Lee Sen <ilkaysen18@gmail.com>
2026-09-20 22:09:40 +0300

    Update test_fixture_db.py

--- Habits/tests/test_fixture_db.py
+++ Habits/tests/test_fixture_db.py
@@ -27,7 +27,7 @@ from modules.analytics import (
 
 
 def get_test_connection(db_name: str = "test_fixture_db"):
-    return sqlite3.connect(test_fixture_db)
+    return sqlite3.connect(db_name)
 
 
 def initialize_test_tables(test_fixture_db):
